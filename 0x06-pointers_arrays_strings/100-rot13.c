7da91488255ddb12c96ff059616f86079d62bbed
Emma0147 <mromotoriogunzabere@gmail.com>
2022-09-21 15:45:05 -0700

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - converts string to leet
- *
- * @c: character string pointer
- * Return: char pointer
+ * rot13 -  a function that encodes a string using rot13.
+ * @s: An input string to encode using rot13
+ * Return: An encode string
  */
-char *rot13(const char *src)
+char *rot13(char *s)
 {
-	int i, j;
-	char rot_13[] =
-		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
-		alphabet[] = 
-			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int i = 0;
 
-	for (i = 0; c[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if (alphabet[j] == c[i])
-			{
-				c[i] = rot_13[j];
-				break;
-			}
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
 		}
+		i++;
 	}
-	return (c);
+	return (s);
 }

8dbb36781337dcd97232f41778ee21f96ab3c760
Emma0147 <mromotoriogunzabere@gmail.com>
2022-09-21 14:50:34 -0700

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,8 @@ char *rot13(char *c)
 {
 	int i, j;
 	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
-		alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		alphabet[] = 
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	for (i = 0; c[i] != '\0'; i++)
 	{

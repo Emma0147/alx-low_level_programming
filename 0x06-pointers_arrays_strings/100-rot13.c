84b22baea07da713bfaae93a0e73e7ef17f58ebb
Emma0147 <mromotoriogunzabere@gmail.com>
2022-09-21 16:03:47 -0700

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - encrypts code
+ * @s: string to encrypt
+ * Return: char value
  */
 char *rot13(char *s)
 {
-	int i = 0;
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+		char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+		int i;
+		int j = 0;
 
-	while (s[i] != '\0')
-	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		for (i = 0; s[i] != '\0'; i++)
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			for (j = 0; part1[j] != '\0'; i++)
+			{
+				if (s[i] == part1[j])
+				{
+					s[i] = part2[j];
+					break;
+				}
+			}
 		}
-		i++;
-	}
-	return (s);
+		return (s);
 }

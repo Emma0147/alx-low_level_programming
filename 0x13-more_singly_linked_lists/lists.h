3295e34e6a5e689ba17c92d23a3044ac710a0ecb
Emma0147 <mromotoriogunzabere@gmail.com>
2022-10-25 14:49:02 -0700

    Write a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,9 +10,8 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for for ALX project
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -32,7 +31,6 @@ typedef struct listp_s
 	struct listp_s *next;
 } listp_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -46,5 +44,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
